5c13f28b27fe7791c9147ec3d9be71e9d5360884
Yuri Gorokhov <yurigorokhov@gmail.com>
2016-06-27 14:23:44 -0700

    Adding README file

--- src/bin.rs
+++ src/bin.rs
@@ -6,20 +6,13 @@ use std::io;
 pub fn main() {
 
     // parse redshift file from stdin
-    let mut redshift_reader = redshift::reader::Reader::new(io::stdin());
+    let redshift_reader = redshift::reader::Reader::new(io::stdin());
 
     // create a writer to stdout
     let mut csv_writer = csv::Writer::from_writer(io::stdout());
 
     // write out each record
-    loop {
-        match redshift_reader.next() {
-            Some(r) => csv_writer.encode(r.values).unwrap(),
-            None => break,
-        }
+    for row in redshift_reader {
+        csv_writer.encode(row.values).unwrap();
     }
-
-//    for row in redshift_reader {
- //       csv_writer.encode(row.values).unwrap();
-  //  }
 }
