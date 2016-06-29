9d16c7624ede84c008de083d687c976ddc7133ee
Yuri Gorokhov <yurigorokhov@gmail.com>
2016-06-29 16:27:44 -0700

    Adding writer

--- src/writer.rs
+++ src/writer.rs
@@ -6,23 +6,53 @@ pub struct ColumnDefinition<'a, T> {
     pub extract_column: Box<(Fn (&T) -> String)>,
 }
 
-pub fn write_columns<W: Write, T>(mut writer: W, column_definitions: Vec<ColumnDefinition<T>>, items: Vec<T>) -> Result<(), Box<Error>> {
+pub struct Writer {
+    output_header: bool,
+}
 
-    // encode each item and write it out to the stream
-    for item in &items {
+impl Writer {
 
-        // for each column, extract the column value from the item
-        for i in 0..column_definitions.len() {
-            let escaped = escape_column_value((column_definitions[i].extract_column)(item));
-            try!(writer.write(escaped.as_bytes()));
-            if i < column_definitions.len() - 1 {
-                try!(writer.write(b"|"));
-            }
+    pub fn new<'a>() -> Writer {
+        Writer {
+            output_header: false,
         }
-        try!(writer.write(b"\n"));
-        try!(writer.flush());
     }
-    Ok(())
+
+    pub fn with_header(&mut self) -> &mut Writer {
+        self.output_header = true;
+        self
+    }
+
+    pub fn write_columns<W: Write, T>(&self, mut writer: W, column_definitions: Vec<ColumnDefinition<T>>, items: Vec<T>) -> Result<(), Box<Error>> {
+
+        // write headers
+        if self.output_header {
+            for i in 0..column_definitions.len() {
+                let escaped = escape_column_value(String::from(column_definitions[i].name));
+                try!(writer.write(escaped.as_bytes()));
+                if i < column_definitions.len() - 1 {
+                    try!(writer.write(b"|"));
+                }
+            }
+            try!(writer.write(b"\n"));
+        }
+
+        // encode each item and write it out to the stream
+        for item in &items {
+
+            // for each column, extract the column value from the item
+            for i in 0..column_definitions.len() {
+                let escaped = escape_column_value((column_definitions[i].extract_column)(item));
+                try!(writer.write(escaped.as_bytes()));
+                if i < column_definitions.len() - 1 {
+                    try!(writer.write(b"|"));
+                }
+            }
+            try!(writer.write(b"\n"));
+            try!(writer.flush());
+        }
+        Ok(())
+    }
 }
 
 fn escape_column_value(value: String) -> String {
--- tests/writer_tests.rs
+++ tests/writer_tests.rs
@@ -38,11 +38,48 @@ mod test {
 
         // Act
         let mut byte_vec: Vec<u8> = Vec::new();
-        let res = redshift::writer::write_columns(&mut byte_vec, test_column_definitions, items);
+        let mut writer = redshift::writer::Writer::new();
+        let res = writer.write_columns(&mut byte_vec, test_column_definitions, items);
 
         // Assert
         assert!(res.is_ok());
         let result_str = str::from_utf8(&byte_vec).unwrap();
         assert_eq!("\"a1\"|\"b1\"|\"c1\"\n\"a2\"|\"b2\"|\"c2\"\n\"a3\"|\"b3\"|\"c3\"\n", result_str);
+    }
+
+    #[test]
+    fn basic_writer_with_header() {
+
+        // Arrange
+        let test_column_definitions = vec![
+            redshift::writer::ColumnDefinition::<TestItem> {
+                name:  "Acolumn",
+                extract_column: Box::new(move |i: &TestItem| i.A.to_string().clone()),
+            },
+            redshift::writer::ColumnDefinition::<TestItem> {
+
+                name: "Bcolumn",
+                extract_column: Box::new(move |i: &TestItem| i.B.to_string().clone()),
+            },
+            redshift::writer::ColumnDefinition::<TestItem> {
+                name: "Ccolumn",
+                extract_column: Box::new(move |i: &TestItem| i.C.to_string().clone()),
+            },
+        ];
+        let items = vec![
+            TestItem { A: "a1", B: "b1", C: "c1" },
+            TestItem { A: "a2", B: "b2", C: "c2" },
+            TestItem { A: "a3", B: "b3", C: "c3" },
+        ];
+
+        // Act
+        let mut byte_vec: Vec<u8> = Vec::new();
+        let mut writer = redshift::writer::Writer::new();
+        let res = writer.with_header().write_columns(&mut byte_vec, test_column_definitions, items);
+
+        // Assert
+        assert!(res.is_ok());
+        let result_str = str::from_utf8(&byte_vec).unwrap();
+        assert_eq!("\"Acolumn\"|\"Bcolumn\"|\"Ccolumn\"\n\"a1\"|\"b1\"|\"c1\"\n\"a2\"|\"b2\"|\"c2\"\n\"a3\"|\"b3\"|\"c3\"\n", result_str);
     }
 }
